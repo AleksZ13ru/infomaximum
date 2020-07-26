aedc6e644208bf0297c73afca746d5efcaa16604
AleksZ13ru <a.zaykin@list.ru>
2020-07-26 03:02:45 +0300

    create apollo query and mutation

--- src/components/Sidenav/Sidenav.js
+++ src/components/Sidenav/Sidenav.js
@@ -5,9 +5,13 @@ import Menu from "../Icon/Menu.svg";
 import User from "../Icon/User.svg";
 import Pie from "../Icon/Pie.svg";
 import {Link} from "react-router-dom";
+import {AUTH_TOKEN} from "../../constants";
+import {useHistory} from "react-router-dom";
 
 export default function Sidenav(props) {
     const {open, onClose} = props;
+    const authToken = localStorage.getItem(AUTH_TOKEN);
+    let history = useHistory();
     return (
         <div id="mySidenav" className="sidenav" style={{width: open ? '224px' : '0'}}>
             <div className="sidebar">
@@ -34,12 +38,24 @@ export default function Sidenav(props) {
                 </Link>
             </div>
             <div className="sidebar" onClick={onClose}>
-                <Link to="/sign-in">
-                    <button>
-                        <img className={"btn-img"} alt={"menu"} src={Pie}/>
-                        <span>Войти</span>
-                    </button>
-                </Link>
+                {authToken ? (
+                    <Link to="#" onClick={() => {
+                        localStorage.removeItem(AUTH_TOKEN);
+                        history.push(`/`)
+                    }}>
+                        <button>
+                            <img className={"btn-img"} alt={"menu"} src={Pie}/>
+                            <span>Выйти</span>
+                        </button>
+                    </Link>
+                ) : (
+                    <Link to="/sign-in" >
+                        <button>
+                            <img className={"btn-img"} alt={"menu"} src={Pie}/>
+                            <span>Войти</span>
+                        </button>
+                    </Link>
+                )}
             </div>
         </div>
     )
